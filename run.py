fbfc9d94d30850258e397b0bcf22523ca560a162
𝗠𝗥 𝗠𝗔𝗙𝗜𝗔 <msrachadleroi01@hotmail.com>
2026-04-18 21:22:35 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,21 +1,15 @@
 import os, sys, platform
-
-try:
-    os.system('git pull')
-except:
-    pass
-
-try:
-    os.system('xdg-open https://www.youtube.com/@mafiams16')
-except:
-    pass
-
+try:os.system('git pull')
+except:pass
+try:os.system('xdg-open https://www.youtube.com/@mafiams16')
+except:pass
 bit = platform.architecture()[0]
-
 try:
     if bit == '64bit':
-        __import__("mrmafia")._____Exception()
+        import mrmafia
+        mrmafia._____Exception()
     else:
-        __import__("mrmafia32")._____Exception()
+        import mrmafia32
+        mrmafia32._____Exception()
 except Exception as e:
-    exit(f"[X] Error: {e}")
+    exit(str(e))
